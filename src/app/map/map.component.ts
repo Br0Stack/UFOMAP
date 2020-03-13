16f986fd80288ada0ae73da0dc8a1033c4bcf74a
Spencer Grier <spenceg85@yahoo.com>
2020-03-13 00:24:47 -0400

    added dynamic ufo api updating in real time

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -15,9 +15,9 @@ import 'leaflet.markercluster/dist/MarkerCluster.Default.css';
 import 'leaflet/dist/images/marker-icon-2x.png';
 import 'leaflet/dist/images/marker-shadow.png';
 //import 'd3';
-declare var require: any
-declare var window: any;
-declare var xmlNode: any;
+declare let require: any
+// declare let window: any;
+// declare let xmlNode: any;
 
 @Component({
   selector: 'app-map',
@@ -28,6 +28,7 @@ export class MapComponent implements OnInit {
   mymap: any;
   clusterGroup: any;
   xmlNode: any;
+  newestAliens: any;
 
   constructor(public sy ? : TopoServiceService) {}
 
@@ -44,46 +45,12 @@ export class MapComponent implements OnInit {
     L.control.zoom({
       position: 'bottomleft'
     }).addTo(this.mymap);
-    var CartoDB_VoyagerOnlyLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}{r}.png', {
+    let CartoDB_VoyagerOnlyLabels = L.tileLayer('https://{s}.basemaps.cartocdn.com/rastertiles/voyager_only_labels/{z}/{x}/{y}{r}.png', {
       attribution: 'www.EdwinGrier.com',
       subdomains: 'abcd',
       maxZoom: 19
     }).addTo(this.mymap);
-    //     L.tileLayer('https://{s}.basemaps.cartocdn.com/dark_all/{z}/{x}/{y}{r}.png', {
-    // 	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>',
-    // 	subdomains: 'abcd',
-    //   maxZoom: 14,
-    //   minZoom: 4
-    // }).addTo(this.mymap);
-    // L.tileLayer('http://www.justicemap.org/tile/{size}/income/{z}/{x}/{y}.png', {
-    // 	attribution: '<a href="http://www.justicemap.org/terms.php">Justice Map</a>',
-    // 	size: 'county',
-    // 	bounds: [[14, -180], [72, -56]]
-    // }).addTo(this.mymap);
-    // L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
-    //   maxZoom: 18,
-    //   attribution: 'Edwin Grier',
-    //   preferCanvas: true,
-    //   id: 'mapbox.streets'
-    // }).addTo(this.mymap);
 
-    //     that.mymap.locate({setView: true, maxZoom: 4});
-
-    //     this.mymap.on('locationfound', onLocationFound);
-    //   function onLocationFound(e) {
-    //     var radius = e.accuracy;
-
-    //     L.marker(e.latlng).addTo(that.mymap)
-    //     .bindPopup('You are here').openPopup();
-    //        // .bindPopup("You are within " + radius + " meters from this job").openPopup();
-
-    //     L.circle(e.latlng, radius).addTo(that.mymap);
-    // }
-    // this.mymap.on('locationerror', onLocationError);
-
-    // function onLocationError(e) {
-    //   alert(e.message);
-    // }
     this.clusterGroup = new L.markerClusterGroup({
       maxClusterRadius: 50,
       spiderfyOnMaxZoom: true,
@@ -91,105 +58,6 @@ export class MapComponent implements OnInit {
       zoomToBoundsOnClick: true,
       //iconCreateFunction: defineClusterIcon
     });
-    // var categoryField = '5074';
-    // var rmax = 30;
-    //test code for D3.js
-    // function defineClusterIcon(cluster) {
-    //   var children = cluster.getAllChildMarkers(),
-    //       n = children.length, //Get number of markers in cluster
-    //       strokeWidth = 1, //Set clusterpie stroke width
-    //       r = rmax-2*strokeWidth-(n<10?12:n<100?8:n<1000?4:0), //Calculate clusterpie radius...
-    //       iconDim = (r+strokeWidth)*2, //...and divIcon dimensions (leaflet really want to know the size)
-    //       data = d3.nest() //Build a dataset for the pie chart
-    //         .key(function(d) { return d.feature.properties[categoryField]; })
-    //         .entries(children, d3.map),
-    //       //bake some svg markup
-    //       html = bakeThePie({data: data,
-    //                           valueFunc: function(d){return d.values.length;},
-    //                           strokeWidth: 1,
-    //                           outerRadius: r,
-    //                           innerRadius: r-10,
-    //                           pieClass: 'cluster-pie',
-    //                           pieLabel: n,
-    //                           pieLabelClass: 'marker-cluster-pie-label',
-    //                           pathClassFunc: function(d){return "category-"+d.data.key;},
-    //                           pathTitleFunc: function(d){return metadata.fields[categoryField].lookup[d.data.key]+' ('+d.data.values.length+' accident'+(d.data.values.length!=1?'s':'')+')';}
-    //                         }),
-    //       //Create a new divIcon and assign the svg markup to the html property
-    //       myIcon = new L.DivIcon({
-    //           html: html,
-    //           className: 'marker-cluster',
-    //           iconSize: new L.Point(iconDim, iconDim)
-    //       });
-    //   return myIcon;
-    // }
-    // function bakeThePie(options) {
-    //   /*data and valueFunc are required*/
-    //   if (!options.data || !options.valueFunc) {
-    //       return '';
-    //   }
-    //   var data = options.data,
-    //       valueFunc = options.valueFunc,
-    //       r = options.outerRadius?options.outerRadius:28, //Default outer radius = 28px
-    //       rInner = options.innerRadius?options.innerRadius:r-10, //Default inner radius = r-10
-    //       strokeWidth = options.strokeWidth?options.strokeWidth:1, //Default stroke is 1
-    //       pathClassFunc = options.pathClassFunc?options.pathClassFunc:function(){return '';}, //Class for each path
-    //       pathTitleFunc = options.pathTitleFunc?options.pathTitleFunc:function(){return '';}, //Title for each path
-    //       pieClass = options.pieClass?options.pieClass:'marker-cluster-pie', //Class for the whole pie
-    //       pieLabel = options.pieLabel?options.pieLabel:d3.sum(data,valueFunc), //Label for the whole pie
-    //       pieLabelClass = options.pieLabelClass?options.pieLabelClass:'marker-cluster-pie-label',//Class for the pie label
-
-    //       origo = (r+strokeWidth), //Center coordinate
-    //       w = origo*2, //width and height of the svg element
-    //       h = w,
-    //       donut = d3.layout.pie(),
-    //       arc = d3.svg.arc().innerRadius(rInner).outerRadius(r);
-
-    //   //Create an svg element
-    //   var svg = document.createElementNS(d3.ns.prefix.svg, 'svg');
-    //   //Create the pie chart
-    //   var vis = d3.select(svg)
-    //       .data([data])
-    //       .attr('class', pieClass)
-    //       .attr('width', w)
-    //       .attr('height', h);
-
-    //   var arcs = vis.selectAll('g.arc')
-    //       .data(donut.value(valueFunc))
-    //       .enter().append('svg:g')
-    //       .attr('class', 'arc')
-    //       .attr('transform', 'translate(' + origo + ',' + origo + ')');
-
-    //   arcs.append('svg:path')
-    //       .attr('class', pathClassFunc)
-    //       .attr('stroke-width', strokeWidth)
-    //       .attr('d', arc)
-    //       .append('svg:title')
-    //         .text(pathTitleFunc);
-
-    //   vis.append('text')
-    //       .attr('x',origo)
-    //       .attr('y',origo)
-    //       .attr('class', pieLabelClass)
-    //       .attr('text-anchor', 'middle')
-    //       //.attr('dominant-baseline', 'central')
-    //       /*IE doesn't seem to support dominant-baseline, but setting dy to .3em does the trick*/
-    //       .attr('dy','.3em')
-    //       .text(pieLabel);
-    //   //Return the svg-markup rather than the actual element
-    //   return serializeXmlNode(svg);
-    // }
-
-
-    // function serializeXmlNode(xmlNode) {
-    //   var s = new XMLSerializer();
-    //   if (typeof window.s != "undefined") {
-    //       return (new window.XMLSerializer()).serializeToString(xmlNode);
-    //   } else if (typeof xmlNode.xml != "undefined") {
-    //       return xmlNode.xml;
-    //   }
-    //   return "";
-    // }
     this.clusterGroup.on('clustermouseover', function (e) {
       e.layer.bindTooltip(e.layer.getAllChildMarkers().length + ' UFO sightings in this area (click)');
       e.layer.openTooltip();
@@ -197,15 +65,12 @@ export class MapComponent implements OnInit {
     var marker = new L.marker([56, -106], {
       opacity: 0.01
     }); //opacity may be set to zero
-    marker.bindTooltip('Map of UFO Sightings Across the World 1940-2015', {
+    marker.bindTooltip('Map of UFO Sightings Across the World', {
       permanent: true,
       className: 'my-label',
       offset: [-200, 80]
     });
     marker.addTo(this.mymap);
-    // var legend = L.control({position: 'topleft'});
-    // legend.addTo(this.mymap) //TO DO
-    //this.getSeries();
   }
 
   ngAfterViewInit() {
@@ -216,111 +81,153 @@ export class MapComponent implements OnInit {
       position: 'left', // left or right
     }).addTo(this.mymap);
 
+    this.makeInitialApiCall()
+  }
 
-    //var require: any
+  makeInitialApiCall() {
+    this.sy.getAliens().subscribe((res: any) => {
+      const alienSightings = res.sightings;
+      this.populateMap(alienSightings, 'api');
+    });
     const jsonURL = require('../../assets/csvjson.json');
-    //console.log(jsonURL)
-    //let endTime = new Date('01/12/2010 12:00:00 AM').toUTCString();
-    for (let a = 0; a < jsonURL.length; a++) {
-      let city = jsonURL[a].city;
-      let comments = jsonURL[a].comments;
-      let country = jsonURL[a].country;
-      let date = jsonURL[a].datetime;
-      // var duration = jsonURL[a].duration
-      let lat = jsonURL[a].latitude
-      let lng = jsonURL[a].longitude
-      let shape = jsonURL[a].shape
-      //var state = jsonURL[a].state
-      //var datetimeEnd = "12/23/2004 07:15"
-      //var endTime = new Date('02/12/2013 12:00:00 AM');
+    this.populateMap(jsonURL, 'json');
+  }
 
-      switch (shape) {
-        case 'fireball':
-          var iconImage = 'assets/img/ufo.png';
-          break;
-        case 'chevron':
-          var iconImage = 'assets/img/chevron.png';
-          break;
-        case 'oval':
-          var iconImage = 'assets/img/oval.png';
-          break;
-        case 'formation':
-          var iconImage = 'assets/img/formation.png';
-          break;
-        case 'sphere':
-          var iconImage = 'assets/img/sphere.png';
-          break;
-        case 'light':
-          var iconImage = 'assets/img/light.png';
-          break;
-        case 'rectangle':
-          var iconImage = 'assets/img/rectangle.png';
-          break;
-        case 'disk':
-          var iconImage = 'assets/img/disk.png';
-          break;
-        case 'changing':
-          var iconImage = 'assets/img/changing.png';
-          break;
-        case 'cigar':
-          var iconImage = 'assets/img/cigar.png';
-          break;
-        case 'circle':
-          var iconImage = 'assets/img/circle.png';
-          break;
-        case 'triangle':
-          var iconImage = 'assets/img/triangle.png';
-          break;
-        case 'cigar':
-          var iconImage = 'assets/img/cigar.png';
-          break;
-        default:
-          var iconImage = 'assets/img/sphere.png';
+  populateMap(alienSightings, type) {
+    alienSightings.forEach(element => {
+      let city, state, summary, duration, date, lat, lng, shape, comments, country = '';
+      if (type === 'api') {
+        console.log(element)
+        city = element.city;
+        state = element.state;
+        summary = element.summary;
+        duration = element.duration;
+        date = element.date;
+        if (element.loc && element.loc[1] && element.loc[0]) {
+          lat = element.loc[1];
+          lng = element.loc[0];
+        }
+        shape = element.shape;
+      } else {
+        city = element.city;
+        comments = element.comments;
+        country = element.country;
+        date = element.datetime;
+        lat = element.latitude;
+        lng = element.longitude;
+        shape = element.shape;
       }
-      var icon = L.icon({
+      let iconImage;
+      switch (shape) {
+        case 'Fireball':
+          iconImage = 'assets/img/ufo.png';
+          break;
+          case 'fireball':
+            iconImage = 'assets/img/ufo.png';
+            break;
+        case 'chevron':
+          iconImage = 'assets/img/chevron.png';
+          break;
+          case 'Chevron':
+            iconImage = 'assets/img/chevron.png';
+            break;
+        case 'oval':
+          iconImage = 'assets/img/oval.png';
+          break;
+          case 'Oval':
+            iconImage = 'assets/img/oval.png';
+            break;
+        case 'formation':
+          iconImage = 'assets/img/formation.png';
+          break;
+          case 'Formation':
+            iconImage = 'assets/img/formation.png';
+            break;
+        case 'sphere':
+          iconImage = 'assets/img/sphere.png';
+          break;
+          case 'Sphere':
+            iconImage = 'assets/img/sphere.png';
+            break;
+        case 'light':
+          iconImage = 'assets/img/light.png';
+          break;
+          case 'Light':
+            iconImage = 'assets/img/light.png';
+            break;
+        case 'rectangle':
+          iconImage = 'assets/img/rectangle.png';
+          break;
+          case 'Rectangle':
+            iconImage = 'assets/img/rectangle.png';
+            break;
+        case 'disk':
+          iconImage = 'assets/img/disk.png';
+          break;
+          case 'Disk':
+            iconImage = 'assets/img/disk.png';
+            break;
+        case 'changing':
+          iconImage = 'assets/img/changing.png';
+          break;
+          case 'Changing':
+            iconImage = 'assets/img/changing.png';
+            break;
+        case 'cigar':
+          iconImage = 'assets/img/cigar.png';
+          break;
+          case 'Cigar':
+            iconImage = 'assets/img/cigar.png';
+            break;
+        case 'circle':
+          iconImage = 'assets/img/circle.png';
+          break;
+          case 'Circle':
+            iconImage = 'assets/img/circle.png';
+            break;
+        case 'triangle':
+          iconImage = 'assets/img/triangle.png';
+          break;
+          case 'Triangle':
+            iconImage = 'assets/img/triangle.png';
+            break;
+        case 'cigar':
+          iconImage = 'assets/img/cigar.png';
+          break;
+          case 'Cigar':
+            iconImage = 'assets/img/cigar.png';
+            break;
+        default:
+          iconImage = 'assets/img/sphere.png';
+      }
+      const icon = L.icon({
         iconUrl: iconImage,
         iconSize: [30, 30],
         iconAnchor: [10, 9]
       });
-      // if( country === 'us' || 'canada' ) {
-      let marker = L.marker([lat, lng], {
-        icon: icon
-      });
-      this.clusterGroup.addLayer(marker);
-      marker.bindPopup('Date: ' + date + ' ' + comments + ' | ' + 'shape:  ' + shape + ' | ' + city);
+      let marker;
+      if (lat && lng !== undefined) {
+        marker = L.marker([lat, lng], {
+          icon: icon
+        });
+        this.clusterGroup.addLayer(marker);
+        if (type === 'api') {
+          marker.bindPopup('Date: ' + date + ' ' + summary + ' | ' + 'shape:  ' + shape +
+          ' | ' + 'duration:  ' + duration + ' | ' + city  + ' | ' + state);
+        } else {
+          marker.bindPopup('Date: ' + date + ' ' + comments + ' | ' + 'shape:  ' + shape + ' | ' + city);
+        }
       marker.on('mouseover', function (e) {
         this.openPopup();
       });
       marker.on('mouseout', function (e) {
         this.closePopup();
       });
-      //}
-
-    }
+      }
+    });
     this.clusterGroup.addTo(this.mymap);
-    //this.lolTest();
-
-    //Test code for the Jobs Map project:
-
-    // getSeries() {
-    //   const seriesBody = {
-    //     seriesid: ['OEUN000000000000015113001'],
-    //     startyear: '2018',
-    //     endyear: '2018',
-    //     catalog: true,
-    //     calculations: false,
-    //     //annualaverage: true,
-    //     registrationkey: '324ee24d5dac410485461df0d4f492db'
-    //   };
-    //      this.sy.getSeries(seriesBody).subscribe(
-    //       data => {
-    //         console.log(data);
-    //       //  this.books = data[0]
-    //       //  this.movies = data[1]
-    //        });
-    // }
-
   }
+
   lolTest() {
     // 'use strict';
     // function logThis() {
--- src/app/topo-service.service.ts
+++ src/app/topo-service.service.ts
@@ -1,7 +1,9 @@
 import { Injectable } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { map, catchError, tap } from 'rxjs/operators';
 
+const endpoint = 'http://ufo-api.herokuapp.com/api/sightings/search?from=2015&to=2021&limit=999999';
 const httpOptions = {
   headers: new HttpHeaders({ 'Content-Type': 'application/json' })
 };
@@ -13,10 +15,13 @@ export class TopoServiceService {
 
   constructor(private http:HttpClient) { }
 
-  getSeries(seriesId) {
-    
-    const body = JSON.stringify(seriesId);
-   
-    return this.http.post('https://api.bls.gov/publicAPI/v2/timeseries/data/', body, httpOptions);
-}
+  private extractData(res: Response) {
+    let body = res;
+    return body || { };
+  }
+  getAliens(): Observable<any> {
+    return this.http.get(endpoint).pipe(
+      map(this.extractData));
+  }
+
 }
